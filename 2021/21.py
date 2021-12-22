2b3a90a7598c18ddb6dc08b8cb71eaab1e128430
Mario Luis Aguayo, Jr <mlaguayojr@gmail.com>
2021-12-21 22:33:30 -0500

    Update 21.py
    added comments

--- 2021/21.py
+++ 2021/21.py
@@ -42,19 +42,24 @@ class Player:
         return "%s currently has %s points" % (self.name, self.points)
 
 def part_one():
-    die = DerterministicDie() # Create the Die to use
+    # Solution for Part One
 
+    # Create Die
+    die = DerterministicDie()
+
+    # Create list of Players
     players = [
-        Player("Player 1", 4, 10, 1, 1000),
-        Player("Player 2", 8, 10, 1, 1000)
+        Player("Player 1", 4, 10, 1, 1000), # Example data for Player 1
+        Player("Player 2", 8, 10, 1, 1000)  # Example data for Player 2
     ]
 
+    # Get first Player object from players list (e.g., "Player 1" since "Player 1" was added first.)
     player = players.pop(0)
 
     loop = True
     while loop:
         others = players
-        print("before rolls:", player)
+        print("before rolls:", player) # Debug for progress before player starts their respective rolls
 
         for i in range(0, 3):
             move = die.Roll()
@@ -64,15 +69,16 @@ def part_one():
         
         player.AddPoints(player.GetPosition())
 
-        print("after rolls:", player)
+        print("after rolls:", player) # Debug for progress before player ends their turn
 
-        if player.Winner():
+        if player.Winner(): # Check if player is winning now
             loop = False
         else:
             players.append(player)
             player = players.pop(0)
 
+    # Iterate through remaining players (e.g., "Player 2", etc.)
     for remaining_player in others:
-        print(remaining_player, die.rollCount, (remaining_player.points * die.rollCount))
+        print(remaining_player, die.rollCount, (remaining_player.points * die.rollCount)) # Debug for player name, how many times the die has been rolled, and expected answer
 
 part_one()
